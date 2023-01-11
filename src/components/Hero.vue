a6408da91fd3686c548578bee6aa0cafd733a952
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-11 16:56:25 +0200

    Hero component created

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="hero is-info">
+    <div class="hero-body">
+      <p class="title">Latest articles about IoT</p>
+      <p class="subtitle">
+        Building the Future with Tech
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
