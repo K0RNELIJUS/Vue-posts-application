f0556a14364dd876517abf8e788afcede82c303c
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-11 16:57:25 +0200

    SinglePostPage view created

---
+++ src/views/SinglePostPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column">
+        <h1 class="title">Single Post Page</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
