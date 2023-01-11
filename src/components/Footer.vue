3ffe05e1d0dce43d24cb937c725993da13b58dbe
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-11 16:56:10 +0200

    Footer component created

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer class="footer is-fullheight mt-auto">
+    <div class="content has-text-centered">
+      <p>
+        Built by
+        <a
+          href="https://lt.linkedin.com/in/kornelijus-irtmonas-frontend-developer"
+          >Kornelijus Irtmonas</a
+        >
+        &copy; {{ year }}
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+
+<style></style>
