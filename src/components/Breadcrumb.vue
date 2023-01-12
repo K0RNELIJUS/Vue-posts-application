a30d2c54eea61b1861f8949bcaca55b96a760f7d
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-12 16:51:41 +0200

    Breadcrumb component created

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="breadcrumb has-arrow-separator" aria-label="breadcrumbs">
+    <ul>
+      <li><a href="/">All Posts</a></li>
+
+      <li class="is-active"><a href="#" aria-current="page">Post Name</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
