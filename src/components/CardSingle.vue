2a07b894893aa9eac17a951600d1aec5f6518923
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-12 16:52:42 +0200

    CardSingle component created

---
+++ src/components/CardSingle.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card">
+    <header class="card-header has-background-light">
+      <p class="card-header-title">
+        {{ post.title }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-6">{{ findAuthor }}</p>
+        </div>
+        <time datetime="2016-1-1">{{ latestDate }}</time>
+      </div>
+
+      <div v-if="isPrimary" class="content">
+        {{ post.body }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <div v-if="edit">
+        <a href="#" class="card-footer-item">Save</a>
+      </div>
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item is-danger">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['isPrimary', 'post', 'authors'],
+  data() {
+    return {
+      edit: false
+    };
+  },
+  methods: {},
+  computed: {
+    findAuthor() {
+      let author = this.authors.filter(
+        author => author.id === this.post.author
+      );
+      return author[0].name;
+    },
+
+    latestDate() {
+      return Date.parse(this.post.updated_at) > Date.parse(this.post.created_at)
+        ? this.post.updated_at
+        : this.post.created_at;
+    }
+  }
+};
+</script>
+
+<style></style>
