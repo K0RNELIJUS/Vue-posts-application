09075264553e6ba8a93aabbfb9d28347c82ac41a
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-13 16:55:47 +0200

    Refactoring/Vuex implemented

--- src/views/PostsPage.vue
+++ src/views/PostsPage.vue
@@ -1,17 +1,16 @@
 <template>
   <section class="section">
-    <Breadcrumb />
     <div class="columns is-flex-wrap-wrap">
-      <div v-if="!posts.length">
+      <div v-if="!allPosts.length">
         <p>No articles posted yet</p>
       </div>
       <div
-        v-for="post in posts"
+        v-for="post in allPosts"
         :key="post.id"
         class="column is-6-tablet is-4-desktop"
       >
         <router-link :to="`/${post.id}`">
-          <Card :isPrimary="false" :authors="authors" :post="post"></Card>
+          <Card :isPrimary="false" :authors="allAuthors" :post="post"></Card>
         </router-link>
       </div>
     </div>
@@ -20,56 +19,23 @@
 </template>
 
 <script>
-import axios from 'axios';
+// Vuex
+import { mapGetters, mapActions } from 'vuex';
+// Components
 import Breadcrumb from '../components/Breadcrumb.vue';
 import Card from '../components/Card.vue';
 import Pagination from '../components/Pagination.vue';
-// import useFetch from '../hooks/useFetch';
-export default {
-  data() {
-    return {
-      authors: [],
-      posts: []
-    };
-  },
-  components: { Breadcrumb, Card, Pagination },
-  methods: {
-    // getData() {
-    //   const { data, loading, error, get, post, put, deleteData } = useFetch(
-    //     'http://localhost:3000/articles',
-    //     'all'
-    //   );
-    //   get();
-    //   console.log(data);
-    //   // this.posts.push(data);
-    // }
-    async fetchData(method, url, id) {
-      switch (method) {
-        case 'getPosts': {
-          try {
-            const { data } = await axios.get(url);
-            this.posts = data;
-          } catch (error) {
-            console.log(error.message);
-          }
-          break;
-        }
-        case 'getAuthors': {
-          try {
-            const { data } = await axios.get(url);
-            this.authors = data;
-          } catch (error) {
-            console.log(error.message);
-          }
-          break;
-        }
-      }
-    }
-  },
 
-  async created() {
-    this.fetchData('getAuthors', 'http://localhost:3000/authors');
-    this.fetchData('getPosts', 'http://localhost:3000/articles');
+export default {
+  components: { Breadcrumb, Card, Pagination },
+
+  methods: { ...mapActions(['fetchPosts', 'fetchAuthors']) },
+
+  computed: { ...mapGetters(['allPosts', 'allAuthors']) },
+
+  created() {
+    this.fetchAuthors();
+    this.fetchPosts();
   }
 };
 </script>
--- src/views/SinglePostPage.vue
+++ src/views/SinglePostPage.vue
@@ -2,52 +2,37 @@
   <section class="section">
     <div class="columns">
       <div class="column">
-        <CardSingle :isPrimary="true" :authors="authors" :post="post" />
+        <CardSingle
+          :isPrimary="true"
+          :authors="allAuthors"
+          :post="singlePost"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import axios from 'axios';
 import CardSingle from '../components/CardSingle.vue';
+// Vuex
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   components: { CardSingle },
   data() {
     return {
-      id: this.$route.params.id,
-      post: {},
-      authors: []
+      id: this.$route.params.id
     };
   },
   methods: {
-    async fetchData(method, url, id) {
-      switch (method) {
-        case 'getSinglePost': {
-          try {
-            const { data } = await axios.get(`${url}${id}`);
-            this.post = data;
-          } catch (error) {
-            console.log(error.message);
-          }
-          break;
-        }
-        case 'getAuthors': {
-          try {
-            const { data } = await axios.get(url);
-            this.authors = data;
-          } catch (error) {
-            console.log(error.message);
-          }
-          break;
-        }
-      }
-    }
+    ...mapActions(['fetchPost', 'fetchAuthors'])
   },
+
+  computed: { ...mapGetters(['singlePost', 'allAuthors']) },
+
   async created() {
-    this.fetchData('getAuthors', 'http://localhost:3000/authors');
-    this.fetchData('getSinglePost', 'http://localhost:3000/articles/', this.id);
+    this.fetchAuthors();
+    this.fetchPost(this.id);
   }
 };
 </script>
