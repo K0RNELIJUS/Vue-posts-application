68e3a4e6ed69e000ec8982ef7bf8eb60e010db0c
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-01-15 21:03:43 +0200

    Pagination component

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,73 +1,45 @@
 <template>
-  <!-- <nav class="pagination" role="navigation" aria-label="pagination">
-  <a class="pagination-previous">Previous</a>
-  <a class="pagination-next">Next page</a>
-  <ul class="pagination-list">
-    <li>
-      <a class="pagination-link" aria-label="Goto page 1">1</a>
-    </li>
-    <li>
-      <span class="pagination-ellipsis">&hellip;</span>
-    </li>
-    <li>
-      <a class="pagination-link" aria-label="Goto page 45">45</a>
-    </li>
-    <li>
-      <a class="pagination-link is-current" aria-label="Page 46" aria-current="page">46</a>
-    </li>
-    <li>
-      <a class="pagination-link" aria-label="Goto page 47">47</a>
-    </li>
-    <li>
-      <span class="pagination-ellipsis">&hellip;</span>
-    </li>
-    <li>
-      <a class="pagination-link" aria-label="Goto page 86">86</a>
-    </li>
-  </ul>
-</nav> -->
-  <div>
-    <button @click="prevPage" :disabled="currentPage === 1">Prev</button>
-    <button @click="nextPage" :disabled="currentPage === totalPages">
-      Next
-    </button>
-    <ul>
-      <li v-for="item in currentItems" :key="item.id">{{ item.name }}</li>
+  <nav class="pagination is-info" role="navigation" aria-label="pagination">
+    <a class="pagination-previous">Previous</a>
+    <a class="pagination-next">Next page</a>
+    <ul class="pagination-list">
+      <li>
+        <a class="pagination-link" aria-label="Goto page 1">1</a>
+      </li>
+      <li>
+        <span class="pagination-ellipsis">&hellip;</span>
+      </li>
+      <li>
+        <a class="pagination-link" aria-label="Goto page 45">45</a>
+      </li>
+      <li>
+        <a
+          class="pagination-link is-current"
+          aria-label="Page 46"
+          aria-current="page"
+          >46</a
+        >
+      </li>
+      <li>
+        <a class="pagination-link" aria-label="Goto page 47">47</a>
+      </li>
+      <li>
+        <span class="pagination-ellipsis">&hellip;</span>
+      </li>
+      <li>
+        <a class="pagination-link" aria-label="Goto page 86">86</a>
+      </li>
     </ul>
-  </div>
+  </nav>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
-  props: ['posts'],
-  data() {
-    return {
-      currentPage: 1,
-      itemsPerPage: 2,
-      items: this.posts
-    };
-  },
-  computed: {
-    totalPages() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
-    },
-    currentItems() {
-      const start = (this.currentPage - 1) * this.itemsPerPage;
-      const end = start + this.itemsPerPage;
-      return this.items.slice(start, end);
-    }
-  },
   methods: {
-    prevPage() {
-      if (this.currentPage > 1) {
-        this.currentPage--;
-      }
-    },
-    nextPage() {
-      if (this.currentPage < this.totalPages) {
-        this.currentPage++;
-      }
-    }
+    ...mapGetters(['allPosts'])
   }
 };
 </script>
+
+<style></style>
