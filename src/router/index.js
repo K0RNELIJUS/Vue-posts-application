a2e8528ad7f88013d846a7818991e6fc4dd2b6d4
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-01-15 21:04:35 +0200

    Not found page view added

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,30 @@
 import PostsPage from '../views/PostsPage.vue';
 import SinglePostPage from '../views/SinglePostPage.vue';
+import NotFound from '../views/NotFound.vue';
 
-let routes = [
+const routes = [
   {
+    name: 'posts-page',
     path: '/',
     component: PostsPage
   },
   {
-    path: '/:id',
+    name: 'single-post-page',
+    path: '/article/:id',
     component: SinglePostPage
+    // beforeEnter(to) {
+    //   const id = parseInt(to.params.id);
+    //   const articles = allPosts.articles;
+
+    //   const exists = articles.some(article => article.id === id);
+
+    //   if (!exists) return { name: 'not-found' };
+    // }
+  },
+
+  {
+    path: '/:pathMatch(.*)*',
+    component: NotFound
   }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    <div class="columns is-centered">
+      <div class="column is-half is-two-fifths-desktop">
+        <div class="notification is-danger is-light ">
+          <h2 class="title has-text-centered">404 Page not found</h2>
+          <div class="is-flex is-justify-content-center">
+            <router-link class="button" to="/"> Go back</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'not-found'
+};
+</script>
+
+<style></style>
