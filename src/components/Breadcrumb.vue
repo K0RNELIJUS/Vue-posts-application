458498af7c9f172a25969953072bb7e6ee90bf9a
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-01-15 20:53:12 +0200

    Navigation added

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,18 +1,29 @@
 <template>
   <nav
-    class="breadcrumb has-arrow-separator px-5 pt-5 mb-0"
+    class="breadcrumb has-arrow-separator px-6 px-7 pt-5 mb-0"
     aria-label="breadcrumbs"
   >
     <ul>
-      <li><a href="/">All Posts</a></li>
+      <li @click="resetSinglePost">
+        <router-link to="/">All Posts</router-link>
+      </li>
 
-      <li class="is-active"><a href="#" aria-current="page">Post Name</a></li>
+      <li v-if="this.singlePost" class="is-active">
+        <a href="#" aria-current="page">{{ this.singlePost.title }}</a>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  computed: {
+    ...mapGetters(['singlePost'])
+  },
+
+  methods: mapActions(['resetSinglePost'])
+};
 </script>
 
 <style></style>
