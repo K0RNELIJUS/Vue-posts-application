3bb0fe40e82f3019ba3c3ed495a349d8273d6046
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-16 16:57:26 +0200

    Unused imports removed

--- src/views/PostsPage.vue
+++ src/views/PostsPage.vue
@@ -1,42 +1,43 @@
 <template>
-  <section class="section ">
-    <div class="is-flex ">
-      <button class="button is-primary mb-5" @click="openModal">
-        Create new article
-      </button>
-    </div>
-    <div class="columns is-flex-wrap-wrap">
-      <div
-        class="column is-flex is-justify-content-center"
-        v-if="!allPosts.length"
-      >
-        <h4 class="is-size-4">No articles posted yet</h4>
+  <main>
+    <section class="section ">
+      <div class="is-flex ">
+        <button class="button is-primary mb-5" @click="openModal">
+          Create new article
+        </button>
       </div>
-      <div
-        v-for="post in allPosts"
-        :key="post.id"
-        class="column is-6-tablet is-4-desktop"
-      >
-        <router-link :to="`/article/${post.id}`">
-          <Card :authors="allAuthors" :post="post"></Card>
-        </router-link>
+      <div class="columns is-flex-wrap-wrap">
+        <div
+          class="column is-flex is-justify-content-center"
+          v-if="!allPosts.length"
+        >
+          <h4 class="is-size-4">No articles posted yet</h4>
+        </div>
+        <div
+          v-for="post in allPosts"
+          :key="post.id"
+          class="column is-6-tablet is-4-desktop"
+        >
+          <router-link :to="`/article/${post.id}`">
+            <Card :authors="allAuthors" :post="post"></Card>
+          </router-link>
+        </div>
       </div>
-    </div>
-    <Pagination />
-  </section>
+      <Pagination />
+    </section>
+  </main>
 </template>
 
 <script>
 // Vuex
 import { mapGetters, mapActions } from 'vuex';
 // Components
-import Breadcrumb from '../components/Breadcrumb.vue';
 import Card from '../components/Card.vue';
 import Pagination from '../components/Pagination.vue';
 
 export default {
   name: 'posts-page',
-  components: { Breadcrumb, Card, Pagination },
+  components: { Card, Pagination },
 
   methods: { ...mapActions(['fetchPosts', 'fetchAuthors', 'openModal']) },
 
