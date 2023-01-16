805c17c35c680890b73e907099ac2ac53b8ddd4d
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-16 16:56:56 +0200

    Post, Delete operations added

--- src/store/modules/posts.js
+++ src/store/modules/posts.js
@@ -24,14 +24,33 @@ const actions = {
 
   resetSinglePost({ commit }) {
     commit('resetSinglePost');
+  },
+  // Create new post
+  async addPost({ commit }, newPost) {
+    const { data } = await axios.post(
+      'http://localhost:3000/articles',
+      newPost
+    );
+    commit('addNewPost', data);
+  },
+
+  async deletePost({ commit }, id) {
+    await axios.delete(`http://localhost:3000/articles/${id}`);
+    commit('removePost', id);
   }
 };
 
 const mutations = {
-  setPosts: (state, posts) => (state.posts = posts),
-
+  setPosts: (state, posts) => (state.posts = posts.reverse()),
+  //   Single post
   setSinglePost: (state, post) => (state.singlePost = post),
-  resetSinglePost: state => (state.singlePost = {})
+  resetSinglePost: state => (state.singlePost = {}),
+  // Create new post
+  addNewPost: (state, newPost) => state.posts.unshift(newPost),
+  // Delete post
+  removePost: (state, id) => {
+    state.posts = state.posts.filter(post => post.id !== id);
+  }
 };
 
 export default {
