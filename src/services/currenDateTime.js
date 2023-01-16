3878e57bd0271648fe59d6760e7ad6232674d896
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-16 16:55:29 +0200

    currentDateTime utility created

---
+++ src/services/currenDateTime.js
@@ -0,0 +1,18 @@
+let date = new Date();
+let currentDateTime = `${date.getFullYear()}-${(date.getMonth() + 1)
+  .toString()
+  .padStart(2, '0')}-${date
+  .getDate()
+  .toString()
+  .padStart(2, '0')} ${date
+  .getHours()
+  .toString()
+  .padStart(2, '0')}:${date
+  .getMinutes()
+  .toString()
+  .padStart(2, '0')}:${date
+  .getSeconds()
+  .toString()
+  .padStart(2, '0')}`;
+
+export default currentDateTime;
