3538d6b80511f038d5d8a6c7f261c6bfb20cfe9c
K0RNELIJUS <kornelijus.irtmonas@gmail.com>
2023-01-18 12:13:19 +0200

    Search component created

---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="field has-addons mb-5">
+    <p class="control">
+      <input
+        v-model="searchTerm"
+        class="input"
+        type="text"
+        placeholder="Find an article"
+      />
+    </p>
+    <p class="control">
+      <button @click="search" class="button is-primary">
+        Search
+      </button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.searchTerm);
+    }
+  }
+};
+</script>
