357423846c2e72b0e7fe46a849ce3a19ab43c0c2
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-01-22 19:19:17 +0200

    Unused getter removed

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -35,7 +35,7 @@ export default {
       this.clearMessage();
     }
   },
-  computed: mapGetters(['messageState', 'clearMessage']),
+  computed: mapGetters(['messageState']),
   components: {
     Message
   }
