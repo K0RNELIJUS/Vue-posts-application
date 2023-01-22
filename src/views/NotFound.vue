3cd48a4343410ecfd10a0558ef5c6d3d7e9d1a4e
Kornelijus Irtmonas <kornelijus.irtmonas@gmail.com>
2023-01-22 19:19:37 +0200

    Message text changed

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,7 +3,9 @@
     <div class="columns is-centered">
       <div class="column is-half is-two-fifths-desktop">
         <div class="notification is-danger is-light ">
-          <h2 class="title has-text-centered">404 Page not found</h2>
+          <h2 class="title has-text-centered">
+            Sorry, this page does not exist!
+          </h2>
           <div class="is-flex is-justify-content-center">
             <router-link class="button" to="/"> Go back</router-link>
           </div>
